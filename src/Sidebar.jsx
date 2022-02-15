1776fda837de1c44c48d97f418eb876051cdec30
terryszhou <terryszhou@gmail.com>
2022-02-15 09:22:38 -0600

    Theme extended

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,8 @@
 export const Sidebar = () => {
   return (
-    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 
+                    flex flex-col shadow-lg bg-primary 
+                    text-secondary">
       <i>A</i>
       <i>B</i>
       <i>C</i>
