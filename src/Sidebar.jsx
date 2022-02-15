81426474c15bcd1b91c5bcaa5fbaa0b7b7f8d684
terryszhou <terryszhou@gmail.com>
2022-02-15 09:51:44 -0600

    tooltips added with @apply and group hover properties

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -13,8 +13,11 @@ export const Sidebar = () => {
   );
 };
 
-const SideBarIcon = ({ icon }) => (
-  <div className="sidebar-icon">
+const SideBarIcon = ({ icon, text = "tooltip" }) => (
+  <div className="sidebar-icon group">
     {icon}
+    <span className="sidebar-tooltip group-hover:scale-100">
+      {text}
+    </span>
   </div>
 );
